7039b07c5149c9d757d4909dc9afa79dfda8e44f
luispinto07 <98784289+luispinto07@users.noreply.github.com>
2022-02-01 21:24:23 +0000

    Login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,59 +1,52 @@
-import './App.css';
+
 import '../login/login.css'
 import React, { useState } from 'react';
 
 
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            }
 
 
-  }
-  
-  render() {
-    return (
-        <div class="container">
-        <div class="screen">
-            <div class="screen__content">
-                <form class="login">
-                    <div class="login__field">
-                        <i class="login__icon fas fa-user"></i>
-                        <input type="text" class="login__input" placeholder="User name / Email"></input>
+    }
+
+    render() {
+        return (
+            <div class="container">
+                <div class="screen">
+                    <div class="screen__content">
+                        <form class="login">
+                            <div class="login__field">
+                                <i class=""></i>
+                                <input type="text" name='' class="login__input" placeholder="Utilizador/Email"></input>
+                            </div>
+                            <div class="login__field">
+                                <i class=""></i>
+                                <input type="password" name='' class="login__input" placeholder="Palavra-Passe"></input>
+                            </div>
+                            <button class="button login__submit">
+                                <span class="button__text">Login</span>
+                                <i class=""></i>
+                            </button>
+                        </form>
+
                     </div>
-                    <div class="login__field">
-                        <i class="login__icon fas fa-lock"></i>
-                        <input type="password" class="login__input" placeholder="Password"></input>
-                    </div>
-                    <button class="button login__submit">
-                        <span class="button__text">Log In Now</span>
-                        <i class="button__icon fas fa-chevron-right"></i>
-                    </button>				
-                </form>
-                <div class="social-login">
-                    <h3>log in via</h3>
-                    <div class="social-icons">
-                        <a href="#" class="social-login__icon fab fa-instagram"></a>
-                        <a href="#" class="social-login__icon fab fa-facebook"></a>
-                        <a href="#" class="social-login__icon fab fa-twitter"></a>
+                    <div class="screen__background">
+                        <span class="screen__background__shape screen__background__shape4"></span>
+                        <span class="screen__background__shape screen__background__shape3"></span>
+                        <span class="screen__background__shape screen__background__shape2"></span>
+                        <span class="screen__background__shape screen__background__shape1"></span>
                     </div>
                 </div>
             </div>
-            <div class="screen__background">
-                <span class="screen__background__shape screen__background__shape4"></span>
-                <span class="screen__background__shape screen__background__shape3"></span>		
-                <span class="screen__background__shape screen__background__shape2"></span>
-                <span class="screen__background__shape screen__background__shape1"></span>
-            </div>		
-        </div>
-    </div>
-    );
-  }
+        );
+    }
 
 }
 
