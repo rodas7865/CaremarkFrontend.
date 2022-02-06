3e9d8bd31bf5b74152a7826933d6b7e7c323e586
rodas7865 <64026265+rodas7865@users.noreply.github.com>
2022-02-06 13:51:46 +0000

    update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,26 +1,61 @@
 
 import React, { useReducer, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import api from '../../Api.js'
 import { withRouter } from '../hooks.js';
+import ('./home.css')
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading:true,
+            hoursOcupied:10,
+            numberColab:12,
+            numberScale:89,
         };
     }
-    
 
+    componentDidMount() {
+        let hoursOcupied=null,
+            numberColab=null,
+            numberScale=null
+
+        const currentDate=new Date(),
+            first=currentDate.getDate()-currentDate.getDay(),
+            last=first+6
+
+        const firstDay=new Date(currentDate.setDate(first)),
+            lastDay=new Date(currentDate.setDate(last))
+
+        console.log(firstDay + " " + lastDay)
+    }
 
     render() {
+        if (this.state.loading) {
+            return (<> loading </>)
+        } else {
+            return (
+                <div className="container">
+                    <h1 className={'Title'}>This week's Performance</h1>
+                    <table className={'Dashboard'}>
+                        <tr>
+                            <th className={'Cell'}>
+                                <h3>Hours Ocupied</h3>
+                                <p className={'expose'}>{this.state.hoursOcupied}</p>
+                            </th>
+                            <th className={'Cell'}>
+                                <h3>Number of Colaborators</h3>
+                                <p className={'expose'}>{this.state.numberColab}</p>
+                            </th>
+                            <th className={'Cell'}>
+                                <h3>Number of Scales</h3>
+                                <p className={'expose'}>{this.state.numberScale}</p>
+                            </th>
+                        </tr>
+                    </table>
 
-        return (
-            <div className="container">
-                
-
-            </div>
-        );
+                </div>
+            );
+        }
     }
 
 }
